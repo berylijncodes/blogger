61a0c24a609f2aaeb6e6ff6bd31db66761c695ab
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-07 11:43:44 +0100

    Create users controller with new  & create actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to article_url(@user), notice: "Welcome #{@user.username}, You have successfully signed up!" }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
